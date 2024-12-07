8e6e6bd297d92e064576b96814550636d772f915
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-07 19:00:17 +0900

    Add venv to .gitignore

--- training.py
+++ training.py
@@ -3,7 +3,14 @@ import random
 import json
 import pickle
 import numpy as np
+import tensorflow as tf
 
 # NLTK stands for natural language toolkit
 import nltk
 from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
+
