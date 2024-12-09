d6b1a3ce75df25efba0f8be08a9fb3e8fce32253
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2024-12-09 17:43:19 +0900

    changing from lists to NumPy arrays

--- training.py
+++ training.py
@@ -36,9 +36,10 @@ words = sorted(set(words))
 classes = sorted(set(classes))
 
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 # Neural network needs numerical values
+# Training the Neural Network
 training = []
 output_empty = [0] * len(classes)
 
@@ -54,14 +55,21 @@ for document in documents:
     training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
-
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+training = np.array(training, dtype=object)
+train_x = np.array(training[:, 0])
+train_y = np.array(training[:, 1])
 
 model = Sequential()
 model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(len(train_y)))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical-crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.model')
+print("Done")
+
